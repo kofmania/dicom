3f6967e81aea3058a813fc8b3b5a6bb4e297d452
Sangho Lee <kofmania@gmail.com>
2025-03-17 13:52:06 +0900

    refactor split render process
    split DICOM viewer initialization and rendering logic

--- src/DicomViewer.tsx
+++ src/DicomViewer.tsx
@@ -12,68 +12,63 @@ const engineId = "myEngine";
 const viewportId = "myView";
 
 const DicomViewer = () => {
-  const viewportRef = useRef(null);
+  const viewportRef = useRef<HTMLDivElement>(null);
   const isFirst = useRef(true);
   const rendering = useRef(false);
 
   useEffect(() => {
+    if (isFirst.current) {
+      csInit();
+      loaderInit({
+        maxWebWorkers: navigator.hardwareConcurrency || 1,
+      });
+      const renderingEngine = new RenderingEngine(engineId);
+      const viewportInput = {
+        viewportId,
+        type: ViewportType.STACK,
+        element: viewportRef.current!,
+        defaultOptions: {
+          background: [0.2, 0, 0.2] as Types.Point3,
+        },
+      };
+
+      renderingEngine.enableElement(viewportInput);
+      isFirst.current = false;
+    }
+  }, []);
+
+  useEffect(() => {
     const initializeCornerstone = async () => {
-      const element = viewportRef.current;
-
-      if (element) {
-        if (isFirst.current) {
-          csInit();
-          loaderInit({
-            maxWebWorkers: navigator.hardwareConcurrency || 1,
-          });
-          isFirst.current = false;
-          const renderingEngine = new RenderingEngine(engineId);
-          const viewportInput = {
-            viewportId,
-            type: ViewportType.STACK,
-            element,
-            defaultOptions: {
-              background: [0.2, 0, 0.2] as Types.Point3,
-            },
-          };
-
-          renderingEngine.enableElement(viewportInput);
-        }
+      const ctImageIds = [
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000000.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000001.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000002.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000003.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000004.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000005.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000006.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000007.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000008.dcm",
+        "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000009.dcm",
+      ];
+      if (!rendering.current) {
+        rendering.current = true;
 
         const renderingEngine = getRenderingEngine(engineId);
-        const viewport = renderingEngine?.getViewport(
+        console.assert(renderingEngine);
+        const viewport = renderingEngine!.getViewport(
           viewportId
         ) as Types.IStackViewport;
-        const ctImageIds = [
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000000.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000001.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000002.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000003.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000004.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000005.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000006.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000007.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000008.dcm",
-          "wadouri:https://ohif-assets-new.s3.us-east-1.amazonaws.com/ACRIN-Regular/CT+CT+IMAGES/CT000009.dcm",
-        ];
 
-        if (!rendering.current) {
-          rendering.current = true;
-          try {
-            const t = Date.now();
-            console.time(`render_${t}`);
-            const res = await viewport.setStack(ctImageIds);
+        console.time("setStack");
+        await viewport.setStack(ctImageIds);
+        console.timeEnd("setStack");
+        console.time("render");
+        viewport.render();
+        console.timeEnd("render");
 
-            console.log(res);
-            viewport.render();
-            console.timeEnd(`render_${t}`);
-          } catch (error) {
-            console.log(error);
-          } finally {
-            rendering.current = false;
-          }
-        }
-      }
+        rendering.current = false;
+      } else console.log("rendering");
     };
 
     initializeCornerstone();
