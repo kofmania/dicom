78a61544e86caad01376708985536554e0c50589
Sangho Lee <kofmania@gmail.com>
2025-03-20 09:46:13 +0900

    refactor: fix lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import DicomTools from "./DicomTools";
-import DicomViewer from "./DicomViewer";
 
 function App() {
   return (
     <>
       <h1>DICOM test</h1>
-      {/* <DicomViewer /> */}
       <DicomTools />
     </>
   );
